df1913d14959d141d9d0c270f2522604f0813aa2
Zarin Loosli <zloosli@byu.edu>
2023-09-14 20:50:39 -0600

    Write clientDefine

---
+++ src/client.mjs
@@ -0,0 +1,46 @@
+import SpaceBridgeCollisionError from "./SpaceBridgeCollisionError.mjs";
+import {
+  executeFunctionRemotely,
+  functionMap,
+  shouldRunLocally,
+} from "./internals.mjs";
+
+/**
+ * @template {Function} RegisteredFunction
+ * @param {string} name the name of the function
+ * @param {RegisteredFunction} func the user-defined function to be wrapped in SpaceBridge logic
+ * @param {SpaceBridgeOptions} [options] -overrides for the global SpaceBridge options
+ *
+ * @returns {RegisteredFunction} a promise-wrapped function
+ *
+ * @throws {SpaceBridgeCollisionError} if a function with the same name has already been registered
+ */
+export function serverDefine(
+  name,
+  func,
+  options = { prefix: "spacebridge", stats: true }
+) {
+  if (functionMap[name]) throw new SpaceBridgeCollisionError(name);
+  functionMap[name] = {
+    callback: func,
+    options,
+  };
+
+  /**
+   * @this {{
+   *  runLocal: Function;
+   *  runRemote: Function;
+   * }}
+   * @param  {...any} args
+   */
+  const registeredFunction = async function (...args) {
+    if (shouldRunLocally()) return await this.runLocal();
+
+    return await this.runRemote(name, ...args);
+  };
+
+  registeredFunction.runLocal = func;
+  registeredFunction.runRemote = executeFunctionRemotely;
+
+  return registeredFunction;
+}
--- src/internals.mjs
+++ src/internals.mjs
@@ -7,3 +7,11 @@
  * }}
  */
 export let functionMap = {};
+
+export function shouldRunLocally() {
+  return true;
+}
+
+export async function executeFunctionRemotely(name, ...args) {
+  return Promise.resolve("foobar");
+}
