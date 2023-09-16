14e5e5bc0f7d8997c917b3f515904e93a1726dde
Zarin Loosli <zloosli@byu.edu>
2023-09-16 16:38:26 -0600

    Fix errors on d.ts

--- src/space-bridge.d.ts
+++ src/space-bridge.d.ts
@@ -3,7 +3,7 @@ type Factor = "network" | "specs" | "cost" | "performance";
 interface InitOptions {
   baseUrl?: string;
   weights?: {
-    [key: Factor]: number;
+    [key in Factor]: number;
   };
   headers?: Object;
   body?: Object;
