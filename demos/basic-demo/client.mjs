3de90eb19d825b1483c33749f8371cd076592093
Zarin Loosli <zloosli@byu.edu>
2023-09-23 11:26:47 -0600

    Make client-side demo better

--- demos/basic-demo/client.mjs
+++ demos/basic-demo/client.mjs
@@ -2,5 +2,8 @@ import { setOptions } from "../../src/index.mjs";
 import { sum } from "./library.mjs";
 
 setOptions({ baseUrl: "https://my-app.com" });
-
 const total = sum(3.14, 6.28);
+const total2 = sum.runLocal(3.14, 6.28);
+
+console.log(await total);
+console.log(total2);
